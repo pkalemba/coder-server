abe356c5791106540078d7a269d80e5155b34173
Pawel Kalemba <pawel.kalemba@corp.ovh.com>
2019-07-04 08:47:50 +0200

    feat(sync): Setup sync from gist
    Sync from gist
    
    Issue: #1

---
+++ entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+if [[ -v GIST_TOKEN]] && [[ -v GIST_ID ]]; then
+    cat <<EOF > /home/coder/.local/share/code-server/User/syncLocalSettings.json
+    {
+        "ignoreUploadFiles": [
+            "state.*",
+            "syncLocalSettings.json",
+            ".DS_Store",
+            "sync.lock",
+            "projects.json",
+            "projects_cache_vscode.json",
+            "projects_cache_git.json",
+            "projects_cache_svn.json",
+            "gpm_projects.json",
+            "gpm-recentItems.json"
+        ],
+        "ignoreUploadFolders": [
+            "workspaceStorage"
+        ],
+        "ignoreExtensions": [],
+        "gistDescription": "Visual Studio Code Settings Sync Gist",
+        "version": 329,
+        "token": "$GIST_TOKEN",
+        "downloadPublicGist": true,
+        "supportedFileExtensions": [
+            "json",
+            "code-snippets"
+        ],
+        "openTokenLink": true,
+        "disableUpdateMessage": false,
+        "lastUpload": null,
+        "lastDownload": null,
+        "githubEnterpriseUrl": null,
+        "askGistName": false,
+        "customFiles": {},
+        "hostName": null
+    }
+    EOF
+    cat <<EOF > /home/coder/.local/share/code-server/User/settings.json
+    {
+        "sync.autoDownload": true,
+        "sync.gist": "$GIST_ID"
+    }
+    EOF
+fi
+
+code-server $PWD $@
+
+
+
--- scripts/install-tools.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt update
-apt upgrade -y
-apt install zsh tmux -y
-git clone
